349c790f36edb581a6f03715c8ac654b7f5e75ae
dodik337 <mricekanal@gmail.com>
2022-11-27 19:46:44 +0300

    база

--- sql_db.sql
+++ sql_db.sql
@@ -3,3 +3,9 @@ id integer primary key autoincrement,
 title text not null,
 url text not null
 );
+
+create table if not exists post (
+id integer primary key autoincrement,
+name not null,
+post not null
+);
