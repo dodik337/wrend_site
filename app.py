b2697286fb63b7ddaa0820fc5fdbca2bf0c41e51
mrice <mricekanal@gmail.com>
2022-12-14 23:13:12 +0300

    Сайт для майнкрафт сервера WrendliWibe

--- app.py
+++ app.py
@@ -5,12 +5,7 @@ app = Flask(__name__)
 
 @app.route('/index')
 def index():
-
     return render_template('index.html', title='1')
-
-@app.errorhandler(404)
-def page_not_found(error):
-    return render_template('page404.html', title='Страница не найдена')
 
 if __name__ == '__main__':
     app.run(debug=True)
